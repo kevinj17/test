5ef35ac9393b0c935789b21647ec12c318364bdd
kevinj17 <kevinjiang17@gmail.com>
2016-05-25 01:44:54 -0400

    Add files via upload

---
+++ src/client_id.js
@@ -0,0 +1,17 @@
+import uuid from 'uuid';
+
+
+export default function getClientId() {
+
+  let id = localStorage.getItem('clientId');
+
+  if (!id) {
+
+    id = uuid.v4();
+
+    localStorage.setItem('clientId', id);
+
+  }
+
+  return id;
+}
